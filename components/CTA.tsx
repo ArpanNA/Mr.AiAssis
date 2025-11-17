d4415f92b9b5d0c1cea294feb438b58e6b6c0bf3
ArpanNA <134919788+ArpanNA@users.noreply.github.com>
2025-11-17 16:12:37 +0530

    Refactored new form functionality

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,15 +1,15 @@
-import { Link } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Cta = () => {
   return (
     <section className="cta-section">
-      <div className="cta-badge">Start learning your way</div>
+      <div className="cta-badge">Start learning your way.</div>
       <h2 className="text-3xl font-bold">
         Build and Personalize Learning Companion
       </h2>
       <p>
-        Pick a name, subject, voice & personality - and start learning through
+        Pick a name, subject, voice, & personality — and start learning through
         voice conversations that feel natural and fun.
       </p>
       <Image src="images/cta.svg" alt="cta" width={362} height={232} />
@@ -22,5 +22,4 @@ const Cta = () => {
     </section>
   );
 };
-
 export default Cta;
