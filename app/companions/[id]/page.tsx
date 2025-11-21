93b64a616b2005b207f09f67f1fc86f3c45a2546
ArpanNA <134919788+ArpanNA@users.noreply.github.com>
2025-11-21 16:28:25 +0530

    error everywere

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,5 +1,6 @@
+import CompanionComponent from "@/components/CompanionComponent";
 import { getCompanion } from "@/lib/actions/companion.actions";
-import { getSubjectColors } from "@/lib/utils";
+import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { redirect } from "next/navigation";
@@ -10,8 +11,10 @@ interface CompanionSessionPageProps {
 
 const companionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const { name, subject, title, topic, duration } = await getCompanion(id);
+  const companion = await getCompanion(id);
   const user = await currentUser();
+
+  const { name, subject, title, topic, duration } = companion;
 
   if (!user) redirect("/sign-in");
   if (!name) redirect("/companions");
@@ -21,7 +24,7 @@ const companionSession = async ({ params }: CompanionSessionPageProps) => {
         <div className="flex items-center gap-2">
           <div
             className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
-            style={{ backgroundColor: getSubjectColors(subject) }}
+            style={{ backgroundColor: getSubjectColor(subject) }}
           >
             <Image
               src={`icons/${subject}.svg`}
@@ -42,6 +45,12 @@ const companionSession = async ({ params }: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
     </main>
   );
 };
